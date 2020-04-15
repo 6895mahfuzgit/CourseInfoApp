3033ad00e2f306ee579a00071294794cf51a39af
mahfuzroyex123 <mahfuzroyex123@gmail.com>
2020-04-15 23:51:59 +0600

    Initial Commot For CourseInfo.Mvc Project

---
+++ CourseInfoApp/CourseInfoApp.Mvc/Data/Migrations/20200415173614_Initial Identity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseInfoApp.Mvc.Data.Migrations
+{
+    public partial class InitialIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
